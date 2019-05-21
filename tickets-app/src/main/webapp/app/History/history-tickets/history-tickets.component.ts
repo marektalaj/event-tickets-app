8f924112e626ba9c599c787a7d86ecbadfb215ac
marek-talaj <marekt1707@gmail.com>
2019-05-21 09:43:39 +0200

    Site with tickets from order done

--- tickets-app/src/main/webapp/app/History/history-tickets/history-tickets.component.ts
+++ tickets-app/src/main/webapp/app/History/history-tickets/history-tickets.component.ts
@@ -1,20 +1,46 @@
 import { Component, OnInit } from '@angular/core';
 import { IOrder } from 'app/shared/model/order.model';
 import { ActivatedRoute } from '@angular/router';
+import { ITicket } from 'app/shared/model/ticket.model';
+import { HttpErrorResponse, HttpResponse } from '@angular/common/http';
+import { filter, map } from 'rxjs/operators';
+import { OrderService } from 'app/entities/order';
+import { JhiAlertService } from 'ng-jhipster';
 
 @Component({
     selector: 'jhi-order-tickets',
-    templateUrl: './order-tickets.component.html',
+    templateUrl: './history-tickets.component.html',
     styles: []
 })
 export class HistoryTicketsComponent implements OnInit {
     order: IOrder;
+    tickets: ITicket[];
 
-    constructor(protected activatedRoute: ActivatedRoute) {}
+    constructor(
+        protected activatedRoute: ActivatedRoute,
+        protected orderService: OrderService,
+        protected jhiAlertService: JhiAlertService
+    ) {}
 
     ngOnInit() {
         this.activatedRoute.data.subscribe(({ order }) => {
             this.order = order;
+            this.orderService
+                .tickets(this.order.id)
+                .pipe(
+                    filter((res: HttpResponse<IOrder[]>) => res.ok),
+                    map((res: HttpResponse<IOrder[]>) => res.body)
+                )
+                .subscribe(
+                    (res: IOrder[]) => {
+                        this.tickets = res;
+                    },
+                    (res: HttpErrorResponse) => this.onError(res.message)
+                );
         });
+    }
+
+    protected onError(errorMessage: string) {
+        this.jhiAlertService.error(errorMessage, null, null);
     }
 }
--- tickets-app/src/main/webapp/app/History/history-tickets/history-tickets.route.ts
+++ tickets-app/src/main/webapp/app/History/history-tickets/history-tickets.route.ts
@@ -1,9 +1,34 @@
-import { Route } from '@angular/router';
+import { ActivatedRouteSnapshot, Resolve, Route, RouterStateSnapshot } from '@angular/router';
 import { HistoryTicketsComponent } from 'app/History/history-tickets/history-tickets.component';
+import { IOrder, Order } from 'app/shared/model/order.model';
+import { filter, map } from 'rxjs/operators';
+import { Observable, of } from 'rxjs/index';
+import { HttpResponse } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { OrderService } from 'app/entities/order/order.service';
+
+@Injectable({ providedIn: 'root' })
+export class OrderResolve implements Resolve<IOrder> {
+    constructor(private service: OrderService) {}
+
+    resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<IOrder> {
+        const id = route.params['id'] ? route.params['id'] : null;
+        if (id) {
+            return this.service.find(id).pipe(
+                filter((response: HttpResponse<Order>) => response.ok),
+                map((order: HttpResponse<Order>) => order.body)
+            );
+        }
+        return of(new Order());
+    }
+}
 
 export const historyTicketsRoute: Route = {
     path: 'history-tickets/:id',
     component: HistoryTicketsComponent,
+    resolve: {
+        order: OrderResolve
+    },
     data: {
         authorities: [],
         pageTitle: 'order-tickets.title'
